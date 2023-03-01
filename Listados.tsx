15c916da2a5d4130f7c282310c850d2bb79ae236
fedeTourn <fede450@outlook.com>
2023-02-28 22:35:01 -0500

    Commit inicial

---
+++ Listados.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+function Listados(): JSX.Element{
+    return(<Text>Hola Fede, funcione</Text>);
+}
+    
+export default Listados;
+
